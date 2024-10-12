3d703f62ce16b1b135eefc50db2ba475a938ccb6
DaniloSantanaleite <Danilo.baumann@icloud.com>
2024-10-12 10:17:58 -0300

    update

---
+++ aula2.js
@@ -0,0 +1,23 @@
+/*
+const - é uma constante não podemos alterar o valor uma vez atribuido
+let -  podemos alterar mais de uma vez
+var - Não é mais utilizado
+*/
+const numero = 30
+let abacate = `NOME DANILO DE SANTANA LEITE ${numero}`
+
+console.log(abacate)
+
+const usuarioarray = [
+    {
+        nome: "Danilo",
+        idade: 22,
+        civil: false
+    }, {
+        nome: "Gelosid",
+        idade: 23,
+        civil: true
+    }
+]
+
+console.log(usuarioarray.idade)
---
+++ aula3.js
@@ -0,0 +1,32 @@
+/* --------- IF - ELSE  --------- */
+/*
+IF (SE)
+ELSE (SE NÃO)
+
+OPERADORES DE COMPARAÇÃO
+
+> MAIOR QUE 
+< MENOR QUE
+== IGUAL QUE
+=== IGUAL QUE (VALOR E O TIPO SE STR E NUMBERS)
+>= MAIOR OU IGUAL QUE
+<= MENOR OU IGUAL QUE
+*/
+
+const aluno = 6
+
+if(aluno >= 7){
+    console.log("Aprovado")
+} else{
+    console.log("REPROVADO")
+
+}
+
+const saopaulo = 800
+const itaqua = 300
+
+if(saopaulo >= 300){
+    console.log("Cidade grande")
+} else{
+    console.log("Cidade Pequena")
+}
